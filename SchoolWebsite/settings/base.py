4265d302020db320459a0764263c826e7149521f
anigilyatornayapushka <zinchenkobogdan123@gmail.com>
2023-07-29 22:54:12 +0600

    ''

--- SchoolWebsite/settings/base.py
+++ SchoolWebsite/settings/base.py
@@ -66,11 +66,14 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': decouple.config('DB_NAME'),
+        'USER': decouple.config('DB_USER'),
+        'PASSWORD': decouple.config('DB_PASS'),
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
